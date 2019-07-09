0faa6f2833f8c6b1ebb2b9e3bcdba6a9ccf7967f
stp171900 <1412893745@qq.com>
2019-07-09 16:18:25 +0800

    配置router，开启页面 npm start

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 导包
+import ElementUI from 'element-ui'
+// 引入样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入router
+import router from '@router'
+// 全局注册
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+// 导入router
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+// 初始化router对象
+const router = new VueRouter({
+  routes: []
+})
+export default router
