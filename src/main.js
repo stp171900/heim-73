be009e32bb968ff3a4120a2c07687d4adf657fcd
stp171900 <1412893745@qq.com>
2019-07-11 15:34:22 +0800

    配置请求axios

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // 引入router
 import router from './router/router'
+import axios from 'axios'
+Vue.prototype.$ajax = axios
 // 全局注册
 Vue.use(ElementUI)
 Vue.config.productionTip = false
